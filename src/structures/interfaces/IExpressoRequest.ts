bb9530b97321db8365dc0e82e7a574ddc4191ac3
Rogério Munhoz <rogerio.j.munhoz@gmail.com>
2019-11-12 22:18:08 -0300

    Add TQuery to IExpressoRequest

--- src/structures/interfaces/IExpressoRequest.ts
+++ src/structures/interfaces/IExpressoRequest.ts
@@ -1,7 +1,8 @@
 import { Request } from 'express'
 
-export interface IExpressoRequest<TBody = unknown, TParams = unknown> extends Request {
+export interface IExpressoRequest<TBody = unknown, TParams = unknown, TQuery = unknown> extends Request {
   body: TBody,
   params: TParams,
-  onBehalfOf: string | string[] | undefined
+  onBehalfOf?: string | string[],
+  query: TQuery
 }
